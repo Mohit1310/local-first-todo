4314545cf60d76ebdec2d057b3060abeeb8a4524
Mohit1310 <mohitdayama1234@gmail.com>
2025-05-26 13:32:57 +0530

    enabling edit functionality

--- src/components/todo.tsx
+++ src/components/todo.tsx
@@ -1,6 +1,7 @@
 import { cn } from "@/lib/utils";
 import type { TodoType } from "@/types/todo";
 import { Pin, X } from "lucide-react";
+import { Input } from "./ui/input";
 
 const Todo = ({
 	t,
@@ -24,13 +25,33 @@ const Todo = ({
 			}),
 		);
 	};
+
+	const updateTodo = (e: React.ChangeEvent<HTMLInputElement>, id: number) => {
+		setTodoList(
+			todoList.map((t) => {
+				if (t.id === id) {
+					return {
+						id: t.id,
+						text: e.target.value,
+					};
+				}
+				return t;
+			}),
+		);
+	};
+
 	return (
 		<div className="flex items-center justify-between rounded-sm border p-1 group/input hover:bg-accent">
-			<div>{t.text}</div>
+			<Input
+				type="text"
+				value={t.text}
+				onChange={(e) => updateTodo(e, t.id)}
+				className="focus-visible:ring-0 focus-visible:border-none border-none dark:bg-input/0 shadow-none"
+			/>
 			<div className="flex items-center gap-0.5 group-hover/input:opacity-100 opacity-0">
 				<button
 					type="button"
-					className="rounded-xs p-1.5 text-primary hover:bg-primary/40 flex items-center justify-center"
+					className="rounded-xs p-1.5 text-primary hover:bg-primary/40 flex items-center justify-center cursor-pointer"
 					onClick={() => pinTodo(t.id)}
 				>
 					<Pin
@@ -42,7 +63,7 @@ const Todo = ({
 				</button>
 				<button
 					type="button"
-					className="rounded-xs p-1.5 text-destructive hover:bg-destructive/40"
+					className="rounded-xs p-1.5 text-destructive hover:bg-destructive/40 cursor-pointer"
 				>
 					<X className="size-4" />
 				</button>
